73ab669677e0a095499f7cb6b191e9fd7318f0f0
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-21 13:47:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   get '/hometown' do
     "My hometown is __"
   end
-  get '/name' do
-    "My name is __"
+  get '/favorite-song' do
+    "My favorite song is __"
   end
 end
